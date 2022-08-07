c17aaff9e59c4fc94b3bb2a950fae57ef7a6ab45
Richansi chauhan <richansi.delhi@gmail.com>
2022-08-07 18:09:09 +0530

    removed variable import

--- lib/genre.dart
+++ lib/genre.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:funku/persionaldetails.dart';
 import 'package:funku/preference_drink.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:funku/components/components/variables.dart';
+// import 'package:funku/components/components/variables.dart';
 import 'dashboard.dart';
 
 bool _click1 = true;
